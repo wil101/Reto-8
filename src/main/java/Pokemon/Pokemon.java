77f87c5632ed455dfe91eb8a553b8cd72a5f5b9f
wil101 <101955537+wil101@users.noreply.github.com>
2022-08-26 12:03:12 -0500

    se hicieron correciones basicas

--- src/main/java/Pokemon/Pokemon.java
+++ src/main/java/Pokemon/Pokemon.java
@@ -1,9 +1,9 @@
-
 package Pokemon;
 
-public abstract class Pokemon{
-    
+public abstract class Pokemon {
+
     public abstract Pokemon evolucionar() throws NoEvolutionException;
+
     public abstract String gritar();
-    
+
 }
